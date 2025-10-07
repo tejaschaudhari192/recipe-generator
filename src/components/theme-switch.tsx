18115dd3e7afead8c6afe84526cc90d1c34cb7a4
Tejas <jaybalaji192@gmail.com>
2025-10-07 11:59:07 +0530

    feat: header with theme icon switch

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -3,8 +3,13 @@
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
 import { Switch } from '@/components/ui/switch';
+import { Moon, Sun } from 'lucide-react';
 
-export default function ThemeSwitch() {
+interface ThemeSwitchProps {
+  type: 'switch' | 'icon';
+}
+
+export default function ThemeSwitch({ type }: ThemeSwitchProps) {
   const { theme, setTheme, resolvedTheme } = useTheme();
   const [isMounted, setIsMounted] = useState(false);
 
@@ -13,14 +18,34 @@ export default function ThemeSwitch() {
     setIsMounted(true);
   }, []);
 
+  useEffect(() => {
+    document.cookie = `theme=${theme}; path=/`;
+  }, [theme]);
+
   if (!isMounted) return null;
 
   const isDark = resolvedTheme === 'dark';
+  switch (type) {
+    case 'switch':
+      return (
+        <Switch
+          checked={isDark}
+          onCheckedChange={(checked) => setTheme(checked ? 'dark' : 'light')}
+        />
+      );
+    case 'icon':
+      return (
+        <div onClick={() => setTheme(!isDark ? 'dark' : 'light')}>
+          {theme == 'light' ? <Moon /> : <Sun />}
+        </div>
+      );
 
-  return (
-    <Switch
-      checked={isDark}
-      onCheckedChange={(checked) => setTheme(checked ? 'dark' : 'light')}
-    />
-  );
+    default:
+      return (
+        <Switch
+          checked={isDark}
+          onCheckedChange={(checked) => setTheme(checked ? 'dark' : 'light')}
+        />
+      );
+  }
 }
